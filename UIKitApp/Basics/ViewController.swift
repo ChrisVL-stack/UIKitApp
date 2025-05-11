ef89a2e38ff48094f3ce578b723079ad628f9b3d
Chris VL <crisvladimir32@gmail.com>
2025-05-11 17:27:39 -0600

    Initial commit

---
+++ UIKitApp/Basics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Christian Vladimir Garzón Gallardo on 11/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPurple
+    }
+
+
+}
+
